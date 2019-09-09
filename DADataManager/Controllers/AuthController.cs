981adc1b725106fc7d368a6fb9ae9c3c940fe2da
Alex Scarlett <alex.scarlett91@gmail.com>
2019-09-09 00:49:45 -0400

    Create AuthController and DTO For Registering User
    Added IAuthRepository and AuthRepository as
    a service and injected into AuthController's
    constructor. Created Dto for registering a user.
    AuthController has Register method that takes
    in Dto and uses injected AuthRepository to
    make a POST request to "/api/auth/register"
    and register a new user.

---
+++ DADataManager/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using DADataManager.Data;
+using DADataManager.Dtos;
+using DADataManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DADataManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // TODO: validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
