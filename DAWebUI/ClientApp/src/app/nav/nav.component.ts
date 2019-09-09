211b3149708ac71722c91b51c6a24b0d28eb8c4e
Alex Scarlett <alex.scarlett91@gmail.com>
2019-09-09 17:35:41 -0400

    Create Nav Component and Create Service for Authenticating Login
    Created Nav component with an Angular
    template form. Added AuthService for
    authenticating users when they login.
    In our app module, we wired up the
    FormsModule  to our "imports",
    NavComponent to our "declarations",
    and "AuthService" to our "providers".

---
+++ DAWebUI/ClientApp/src/app/nav/nav.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+    model: any = {};
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
+
+    login() {
+        console.log(this.model);
+    }
+
+}
