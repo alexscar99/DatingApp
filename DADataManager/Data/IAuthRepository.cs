785edeee74a76ed125944510f430a471336e6a93
Alex Scarlett <alex.scarlett91@gmail.com>
2019-09-16 20:51:24 -0400

    Remove Unused "Using" Statements

--- DADataManager/Data/IAuthRepository.cs
+++ DADataManager/Data/IAuthRepository.cs
@@ -1,7 +1,4 @@
 ﻿using DADataManager.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DADataManager.Data
--- DADataManager/Startup.cs
+++ DADataManager/Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using DADataManager.Data;
 using DADataManager.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -11,13 +7,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DADataManager
